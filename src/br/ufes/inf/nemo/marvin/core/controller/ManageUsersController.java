ef4e50bf63a1bd5cd536002244591761570f11bf
Flavio <Flavio@Fomalhaut>
2017-05-22 19:24:11 -0300

    First name users(not working)

--- src/br/ufes/inf/nemo/marvin/core/controller/ManageUsersController.java
+++ src/br/ufes/inf/nemo/marvin/core/controller/ManageUsersController.java
@@ -1,7 +1,10 @@
 
 	package br.ufes.inf.nemo.marvin.core.controller;
 
-	import javax.ejb.EJB;
+	import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
 	import javax.enterprise.context.SessionScoped;
 	import javax.inject.Named;
 
@@ -19,8 +22,15 @@
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
-		
+		private User user = new User();
+		private static final Logger logger = Logger.getLogger(ManageUsersController.class.getCanonicalName());
+
 		@EJB private ManageUsersService manageUsersService;
+		
+		public User getUser() {
+			return user;
+		}
+		
 		@Override
 		protected CrudService<User> getCrudService() {
 			return manageUsersService;
@@ -29,6 +39,18 @@
 		@Override
 		protected void initFilters() {
 			addFilter(new LikeFilter("manageUsers.filter.byName", "name", getI18nMessage("msgsCore", "manageUsers.text.filter.byName")));		
+		}
+			
+		public void suggestShortName() {
+			// If the name was filled and the short name is still empty, suggest the first name as short name.
+			String name = user.getName();
+			String shortName = user.getShortName();
+			if ((name != null) && ((shortName == null) || (shortName.length() == 0))) {
+				int idx = name.indexOf(" ");
+				user.setShortName((idx == -1) ? name : name.substring(0, idx).trim());
+				logger.log(Level.FINE, "Suggested \"{0}\" as short name for \"{1}\"", new Object[] { user.getShortName(), name });
+			}
+			else logger.log(Level.FINEST, "Short name not suggested: empty name or short name already filled (name is \"{0}\", short name is \"{1}\")", new Object[] { name, shortName });
 		}
 
 	}
