79c52b19c8f7f6fc5a0858202765dfd9050e4667
root <root@Antares>
2017-07-31 03:16:27 -0300

    ops

--- src/br/ufes/inf/nemo/marvin/core/application/SessionPlaceBean.java
+++ src/br/ufes/inf/nemo/marvin/core/application/SessionPlaceBean.java
@@ -43,6 +43,7 @@ public class SessionPlaceBean implements SessionPlace {
 
 	
 	public void savePlaceSearch(ResultSet results){
+		
 		QuerySolution querySolution = results.next();
 		String city_name = querySolution.get("city_name").toString();
 		String country_name = querySolution.get("country_name").toString();
@@ -55,7 +56,7 @@ public class SessionPlaceBean implements SessionPlace {
 
 		place.setName(city_name);
 		place.setCountry(country_name);
-		place.setDescription(country_name);
+		place.setDescription(description);
 		
 		place.setClimate(climate);
 		place.setHeight(height.getLong());
@@ -64,8 +65,8 @@ public class SessionPlaceBean implements SessionPlace {
 		
 		placeDAO.save(place);
 
-		System.out.println("place.country" + place.getCountry());				
-		System.out.println(querySolution.get("country_name").toString());
+		//System.out.println("place.country" + place.getCountry());				
+		//System.out.println(querySolution.get("country_name").toString());
 		return;
 		}
 
